a3b30f2b4aff54bd7f64d8dc91e9301b54652e8a
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-05 18:07:34 +0000

    Do NOT add public to the int

--- ToDo.cs
+++ ToDo.cs
@@ -2,4 +2,14 @@
 public class ToDo
 {
     // Add your code here
+    public string[] MarkAsDone(string[] todos)
+    {
+        int i = 0;
+        
+        while(i < todos.Length){
+            todos[i] = "done - " + todos[i];
+            i++;
+        }
+        return todos;
+    }
 }
